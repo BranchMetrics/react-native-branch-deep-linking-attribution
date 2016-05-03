36b6cbd4cdec15b15d70148e07901b5372f870aa
Zack Story <ztstory@gmail.com>
2016-05-02 22:43:00 -0700

    [RNBranch.m] do not call [error localizedDescription] if error is a NSString

--- RNBranch/RNBranch.m
+++ RNBranch/RNBranch.m
@@ -25,8 +25,11 @@ RCT_EXPORT_MODULE();
 //Called by AppDelegate.m -- stores initSession result in static variables and raises initSessionFinished event that's captured by the RNBranch instance to emit it to React Native
 + (void)initSessionWithLaunchOptions:(NSDictionary *)launchOptions isReferrable:(BOOL)isReferrable {
   [[Branch getInstance] initSessionWithLaunchOptions:launchOptions isReferrable:isReferrable andRegisterDeepLinkHandler:^(NSDictionary *params, NSError *error) {
-    initSessionWithLaunchOptionsResult = @{@"params": params ? params : [NSNull null], @"error": error ? [error localizedDescription] : [NSNull null]};
-    [[NSNotificationCenter defaultCenter] postNotificationName:initSessionWithLaunchOptionsFinishedEventName object:initSessionWithLaunchOptionsResult]; //Forward to RNBranch instance
+    initSessionWithLaunchOptionsResult = @{@"params": params ? params : [NSNull null], @"error": error ? error : [NSNull null]};
+    if ([initSessionWithLaunchOptionsResult[@"error"] respondsToSelector:@selector(localizedDescription)]) {
+      initSessionWithLaunchOptionsResult[@"error"] = [notificationObject[@"error"] localizedDescription];
+    }
+    [[NSNotificationCenter defaultCenter] postNotificationName:initSessionWithLaunchOptionsFinishedEventName object:initSessionWithLaunchOptionsResult];
   }];
 }
 
