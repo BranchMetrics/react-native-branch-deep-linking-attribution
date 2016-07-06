6a5e2f54d4017e6d88d0bd5d375937e7cb0b2f87
Zack Story <zack@root-two.com>
2016-07-06 00:02:41 -0700

    showShareSheetWithLnkProperties completion renamed to andCallback

--- ios/RNBranch.m
+++ ios/RNBranch.m
@@ -173,13 +173,13 @@ RCT_EXPORT_METHOD(
   dispatch_async(dispatch_get_main_queue(), ^(void){
     BranchUniversalObject *branchUniversalObject = [self createBranchUniversalObject:branchUniversalObjectMap];
     BranchLinkProperties *linkProperties = [self createLinkProperties:linkPropertiesMap withControlParams:controlParamsMap];
-    
+
     UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
     UIViewController *fromViewController = (rootViewController.presentedViewController ? rootViewController.presentedViewController : rootViewController);
     [branchUniversalObject showShareSheetWithLinkProperties:linkProperties
                            andShareText:[shareOptionsMap objectForKey:@"messageBody"]
                            fromViewController:fromViewController
-                           completion:^(NSString *activityType, BOOL completed){
+                           andCallback:^(NSString *activityType, BOOL completed){
       NSDictionary *result = @{
         @"channel" : activityType ? activityType : [NSNull null],
         @"completed" : [NSNumber numberWithBool:completed],
