7aa03445a6c088d316957dcfc26dc26b5797711a
Zack Story <zack@root-two.com>
2016-10-03 15:50:47 +0200

    [testbed] update deps and latest

--- testbed/android/app/src/main/java/com/testbed/MainActivity.java
+++ testbed/android/app/src/main/java/com/testbed/MainActivity.java
@@ -1,16 +1,9 @@
 package com.testbed;
 
 import com.facebook.react.ReactActivity;
+
 import io.branch.rnbranch.*;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
-import java.util.Arrays;
-import java.util.List;
-
 import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends ReactActivity {
 
@@ -23,37 +16,10 @@ public class MainActivity extends ReactActivity {
         return "testbed";
     }
 
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new RNBranchPackage()
-        );
-    }
-
     @Override
     protected void onStart() {
         super.onStart();
         RNBranchModule.initSession(this.getIntent().getData(), this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        RNBranchModule.onStop();
     }
 
     @Override
