fdfd5244e755f32b11aadf24d264c2fd750ccb0d
Jimmy Dee <jgvdthree@gmail.com>
2017-01-11 11:25:52 -0800

    Add Carthage framework to SCM

---
+++ testbed/testbed_carthage/ios/Carthage/Checkouts/ios-branch-deep-linking/Branch-TestBed/Branch-TestBed/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Branch-TestBed
+//
+//  Created by Alex Austin on 6/5/14.
+//  Copyright (c) 2014 Branch Metrics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Branch.h"
+
+@interface ViewController : UITableViewController
+
+@end
