9264d1cc4ace0860c340f3f3a0372e6c5411f311
Jimmy Dee <jgvdthree@gmail.com>
2017-03-28 14:11:32 -0700

    Added share button with call to showShareSheet

--- examples/webview_example/src/Article.js
+++ examples/webview_example/src/Article.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, WebView } from 'react-native'
+import { Button, StyleSheet, View, WebView } from 'react-native'
 
 import branch, { RegisterViewEvent } from 'react-native-branch'
 
@@ -15,11 +15,12 @@ export default class Article extends Component {
 
   async componentWillMount() {
     this.buo = await branch.createBranchUniversalObject("planet:" + this.props.title, {
+      automaticallyListOnSpotlight: true, // ignored on Android
       canonicalUrl: this.props.url,
       title: this.props.title
     })
     this.buo.userCompletedAction(RegisterViewEvent)
-    console.log("Created Branch Universal Object: " + this.buo)
+    console.log("Created Branch Universal Object.")
   }
 
   componentWillUnmount() {
@@ -29,9 +30,33 @@ export default class Article extends Component {
 
   render() {
     return (
-      <WebView
-        style={styles.container}
-        source={{uri: this.props.url}} />
-      )
+      <View
+        style={styles.container} >
+        <WebView
+          source={{uri: this.props.url}} />
+        <Button
+          title="Share"
+          onPress={() => this.onShare()} />
+      </View>
+    )
+  }
+
+  async onShare() {
+    let { channel, completed, error } = await this.buo.showShareSheet({
+      messageHeader: "The Planet " + this.props.title,
+      messageBody: "Read about the planet " + this.props.title + "."
+    }, {
+      feature: "share",
+      channel: "RNApp"
+    }, {
+      $desktop_url: this.props.url
+    })
+
+    if (error) {
+      console.error("Error sharing via Branch: " + error)
+      return
+    }
+
+    console.log("Share to " + channel + " completed: " + completed)
   }
 }
