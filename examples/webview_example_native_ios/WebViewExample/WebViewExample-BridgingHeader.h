afd20d087338e5123ffa795e0b27e2bcc31efb45
Jimmy Dee <jgvdthree@gmail.com>
2017-04-05 16:07:33 -0700

    Some comments

--- examples/webview_example_native_ios/WebViewExample/WebViewExample-BridgingHeader.h
+++ examples/webview_example_native_ios/WebViewExample/WebViewExample-BridgingHeader.h
@@ -6,6 +6,12 @@
 //  Copyright © 2017 Branch Metrics. All rights reserved.
 //
 
+/*
+ * The React pod cannot be built with use_frameworks!. It fails to find a yoga header. To
+ * import modules into Swift from static libraries, all pods are imported into a
+ * bridging header.
+ */
+
 #import <Branch/Branch.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
