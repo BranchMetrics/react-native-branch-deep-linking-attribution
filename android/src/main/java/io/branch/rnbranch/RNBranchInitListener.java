9445eacbe09f230ac96f1e06c789fc6bd88d478e
Jimmy Dee <jgvdthree@gmail.com>
2017-04-11 13:55:57 -0700

    Some javadoc for RNBranchInitListener. Removed redundant public declaration.

--- android/src/main/java/io/branch/rnbranch/RNBranchInitListener.java
+++ android/src/main/java/io/branch/rnbranch/RNBranchInitListener.java
@@ -10,5 +10,12 @@ import io.branch.referral.util.LinkProperties;
  */
 
 public interface RNBranchInitListener {
-    public void onInitFinished(Uri uri, BranchUniversalObject branchUniversalObject, LinkProperties linkProperties, BranchError error);
+    /**
+     * Called when RNBranch.initSession succeeds or fails and whenever a link is opened.
+     * @param uri the original Uri passed to the app (null for initialization responses)
+     * @param branchUniversalObject a BranchUniversalObject representing the link or null
+     * @param linkProperties an instance of LinkProperties representing the link configuration or null
+     * @param error a BranchError indicating an error (null if no error)
+     */
+    void onInitFinished(Uri uri, BranchUniversalObject branchUniversalObject, LinkProperties linkProperties, BranchError error);
 }
