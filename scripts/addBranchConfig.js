7cc711c79bc4aaf942c5c4050e2c0882993a1467
Jimmy Dee <jgvdthree@gmail.com>
2017-06-20 15:03:36 -0700

    Log an info message advertising the option of using branch.json

--- scripts/addBranchConfig.js
+++ scripts/addBranchConfig.js
@@ -1,12 +1,16 @@
 var androidUtil = require('./androidUtil')
 var fs = require('fs')
+var log = require('npmlog')
 var iosUtil = require('./iosUtil')
+
+log.heading = 'react-native-branch'
 
 // TODO: Should it be possible to use just branch.debug.json w/o branch.json?
 // This seems like an unlikely configuration. Maybe the check should just be
 // for branch.json, or if just branch.debug.json exists, a warning should be
 // generated.
 if (!fs.existsSync('./branch.json') && !fs.existsSync('./branch.debug.json')) {
+  log.info('branch.json not found. See https://rnbranch.app.link/branch-json for more information.')
   return
 }
 
