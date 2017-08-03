bc643824f1a5de6cc0bc4b4a798b9ffd47c80027
Jimmy Dee <jgvdthree@gmail.com>
2017-08-03 10:26:36 -0700

    Remove unused import in base class

--- android/src/main/java/io/branch/rnbranch/RNBranchPackageBase.java
+++ android/src/main/java/io/branch/rnbranch/RNBranchPackageBase.java
@@ -3,7 +3,6 @@ package io.branch.rnbranch;
 import java.util.*;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
