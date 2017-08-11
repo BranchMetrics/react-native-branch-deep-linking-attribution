e29faacde2b30680f986fe9ac4eb155bb0f837b5
Jimmy Dee <jgvdthree@gmail.com>
2017-08-11 15:19:32 -0700

    Automatically update Branch-SDK.podspec from submodule

---
+++ fastlane/lib/update_native_sdks.rb
@@ -0,0 +1,37 @@
+module Fastlane
+  module Actions
+    class UpdateNativeSdksAction < Action
+      class << self
+        UI = FastlaneCore::UI
+
+        def run(params)
+          # TODO: Update submodules
+
+          # TODO: Android
+
+          ios_subdir = File.expand_path 'ios', '.'
+          `cp #{ios_subdir}/Branch-SDK/Branch.podspec #{ios_subdir}/Branch-SDK.podspec`
+          UI.user_error! "Unable to update #{ios_subdir}/Branch-SDK.podspec" unless $?.exitstatus == 0
+
+          other_action.apply_patch(
+            files: "#{ios_subdir}/Branch-SDK.podspec",
+            regexp: /(s\.name\s*)= "Branch"/,
+            mode: :replace,
+            text: '\1= "Branch-SDK"'
+          )
+
+          other_action.apply_patch(
+            files: "#{ios_subdir}/Branch-SDK.podspec",
+            regexp: /s.requires_arc.*$/,
+            mode: :append,
+            text: "\n  s.header_dir       = \"Branch\""
+          )
+        end
+
+        def available_options
+          []
+        end
+      end
+    end
+  end
+end
