735e2bdb2fc1aeda2f852b1c276430285577d468
Jimmy Dee <jgvdthree@gmail.com>
2017-08-11 15:34:36 -0700

    Refactoring

--- fastlane/lib/update_native_sdks.rb
+++ fastlane/lib/update_native_sdks.rb
@@ -10,9 +10,19 @@ module Fastlane
           # TODO: Android
 
           ios_subdir = File.expand_path 'ios', '.'
+          update_branch_podspec_from_submodule ios_subdir
+        end
+
+        def available_options
+          []
+        end
+
+        def update_branch_podspec_from_submodule(ios_subdir)
+          # Copy the podspec from the submodule
           `cp #{ios_subdir}/Branch-SDK/Branch.podspec #{ios_subdir}/Branch-SDK.podspec`
           UI.user_error! "Unable to update #{ios_subdir}/Branch-SDK.podspec" unless $?.exitstatus == 0
 
+          # Change the pod name to Branch-SDK
           other_action.apply_patch(
             files: "#{ios_subdir}/Branch-SDK.podspec",
             regexp: /(s\.name\s*)= "Branch"/,
@@ -20,6 +30,7 @@ module Fastlane
             text: '\1= "Branch-SDK"'
           )
 
+          # Add s.header_dir = "Branch" (also determines the module name)
           other_action.apply_patch(
             files: "#{ios_subdir}/Branch-SDK.podspec",
             regexp: /s.requires_arc.*$/,
@@ -27,6 +38,7 @@ module Fastlane
             text: "\n  s.header_dir       = \"Branch\""
           )
 
+          # Add an additional Branch-SDK/ path component to all paths to account for the submodule
           other_action.apply_patch(
             files: "#{ios_subdir}/Branch-SDK.podspec",
             regexp: %r{(['"]Branch-SDK/)},
@@ -34,10 +46,8 @@ module Fastlane
             text: '\1Branch-SDK/',
             global: true
           )
-        end
 
-        def available_options
-          []
+          UI.message "Updated #{ios_subdir}/Branch-SDK.podspec"
         end
       end
     end
