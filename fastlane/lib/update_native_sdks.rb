16994a7b27b238fb174f1d776aee4d5c77bc8e90
Jimmy Dee <jgvdthree@gmail.com>
2017-08-14 15:54:21 -0700

    Corrected nil file_ref issue

--- fastlane/lib/update_native_sdks.rb
+++ fastlane/lib/update_native_sdks.rb
@@ -47,36 +47,87 @@ module Fastlane
         end
 
         def adjust_rnbranch_xcodeproj(ios_subdir)
-          project = Xcodeproj::Project.open "#{ios_subdir}/RNBranch.xcodeproj"
+          @project = Xcodeproj::Project.open "#{ios_subdir}/RNBranch.xcodeproj"
           ios_subdir_pathname = Pathname.new ios_subdir
 
-          Dir[File.expand_path "#{ios_subdir}/Branch-SDK/**/*.[hm]", ios_subdir].each do |header|
-            next if header =~ /Test/ || project.files.find { |f| f.real_path.to_s == header }
+          # 1. Find all SDK .h, .m files and add any not already in the project.
+          Dir[File.expand_path "#{ios_subdir}/Branch-SDK/**/*.[hm]", ios_subdir].each do |filename|
+            # Avoid TestBed and tests. (alternatives: parse the podspec for the source_files or
+            # restructure the ios repo so that the SDK is well separated)
+            # Ignore any files already in the project.
+            next if filename =~ /Test/ || @project.files.find { |f| f.real_path.to_s == filename }
 
             # New file. Look for the group.
-            group_pathname = Pathname.new(File.dirname(header)).relative_path_from(ios_subdir_pathname)
-            group = ensure_group_at_path project, group_pathname
+            group_pathname = Pathname.new(File.dirname(filename)).relative_path_from(ios_subdir_pathname)
+            group = ensure_group_at_path group_pathname
 
-            group.new_file File.basename(header)
+            file_basename = File.basename filename
+            file = group.new_file file_basename
 
-            target = project.targets.first
+            UI.message "adding #{file.path} in #{group.name}: #{file.source_tree}"
+            build_file =
+              if filename =~ /\.h$/
+                copy_branch_sdk_headers_build_phase.add_file_reference file
+              else
+                source_build_phase.add_file_reference file
+              end
+            UI.message "  build_file = #{build_file.display_name}, #{build_file.isa}, #{build_file.uuid}"
+          end
 
-            if header =~ /\.h$/
-              target.header_build_phase
+          # check_file_refs
+
+          # 2. Make sure all files in the project still exist. Remove those that do not.
+          @project.files.each do |file|
+            next if File.exist? file.real_path
+            # file.remove_from_project # results in nil file_ref in build phases
+            file.parent.children.delete file
+            if file.path =~ /\.h$/
+              copy_branch_sdk_headers_build_phase.remove_file_reference file
+            else
+              source_build_phase.remove_file_reference file
             end
           end
 
-          project.save
+          # check_file_refs
+
+          # 3. Remove any empty groups from the project
+          remove_empty_groups @project.main_group
+
+          # check_file_refs
+
+          @project.save
         end
 
-        def ensure_group_at_path(project, pathname)
-          subgroup = project.main_group[pathname.to_s]
+        def ensure_group_at_path(pathname)
+          subgroup = @project.main_group[pathname.to_s]
           return subgroup unless subgroup.nil?
 
           dirname, basename = pathname.split
-          parent = ensure_group_at_path project, dirname
+          parent = ensure_group_at_path dirname
 
           parent.new_group basename.to_s, basename.to_s
+        end
+
+        def remove_empty_groups(group)
+          group.groups.each { |g| remove_empty_groups g }
+          group.remove_from_project if group.empty?
+        end
+
+        def check_file_refs
+          [copy_branch_sdk_headers_build_phase, source_build_phase].each do |build_phase|
+            build_phase.files.each do |build_file|
+              raise "nil file_ref in #{build_phase.display_name}" if build_file.file_ref.nil?
+            end
+          end
+        end
+
+        def copy_branch_sdk_headers_build_phase
+          target = @project.targets.first
+          target.build_phases.find { |phase| phase.respond_to?(:name) && phase.name == "Copy Branch SDK Headers" }
+        end
+
+        def source_build_phase
+          @project.targets.first.source_build_phase
         end
       end
     end
