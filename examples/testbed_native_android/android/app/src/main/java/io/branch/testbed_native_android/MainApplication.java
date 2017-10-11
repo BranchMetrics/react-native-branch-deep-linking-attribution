5179314cb23d9c17054706e7e7e2c767e72eb05b
Jimmy Dee <jgvdthree@gmail.com>
2017-10-11 09:19:02 -0700

    Enable multidexing in testbed_native_android as well

--- examples/testbed_native_android/android/app/src/main/java/io/branch/testbed_native_android/MainApplication.java
+++ examples/testbed_native_android/android/app/src/main/java/io/branch/testbed_native_android/MainApplication.java
@@ -1,6 +1,8 @@
 package io.branch.testbed_native_android;
 
-import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import io.branch.referral.Branch;
 
@@ -8,10 +10,16 @@ import io.branch.referral.Branch;
  * Created by jdee on 3/30/17.
  */
 
-public class MainApplication extends Application {
+public class MainApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
         Branch.getAutoInstance(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
