422d6ab9717f3611b292d422f05ca4c571176a9c
Jimmy Dee <jgvdthree@gmail.com>
2019-05-01 16:26:11 -0700

    try eliminating ListItem

--- examples/webview_tutorial/src/ArticleList.js
+++ examples/webview_tutorial/src/ArticleList.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react'
-import { Text, Image, FlatList, ListItem, StyleSheet, TouchableHighlight, View } from 'react-native'
-
-// Step 1: import Branch
+import { Text, Image, FlatList, StyleSheet, TouchableHighlight, View } from 'react-native'
 
 import Article from './Article'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 0
+    marginTop: 0,
   },
 })
-
-const planetData = [
-  { title: 'Mercury', url: 'https://en.wikipedia.org/wiki/Mercury_(planet)', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg' },
-  { title: 'Venus', url: 'https://en.wikipedia.org/wiki/Venus', image: 'https://upload.wikimedia.org/wikipedia/commons/e/e5/Venus-real_color.jpg' },
-  { title: 'Earth', url: 'https://en.wikipedia.org/wiki/Earth', image: 'https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg' },
-  { title: 'Mars', url: 'https://en.wikipedia.org/wiki/Mars', image: 'https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg' },
-  { title: 'Jupiter', url: 'https://en.wikipedia.org/wiki/Jupiter', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg' },
-  { title: 'Saturn', url: 'https://en.wikipedia.org/wiki/Saturn', image: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Saturn-27-03-04.jpeg' },
-  { title: 'Uranus', url: 'https://en.wikipedia.org/wiki/Uranus', image: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg' },
-  { title: 'Neptune', url: 'https://en.wikipedia.org/wiki/Neptune', image: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Neptune_Full.jpg' },
-  { title: 'Pluto', url: 'https://en.wikipedia.org/wiki/Pluto', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2a/Nh-pluto-in-true-color_2x_JPEG-edit-frame.jpg' }
-]
 
 class ArticleList extends Component {
   // Step 2: Add _unsubscribeFromBranch property
 
   constructor(props) {
     super(props)
-    this.props.planetData = planetData
+
+    this.props.planets = [
+      { title: 'Mercury', url: 'https://en.wikipedia.org/wiki/Mercury_(planet)', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg' },
+      { title: 'Venus', url: 'https://en.wikipedia.org/wiki/Venus', image: 'https://upload.wikimedia.org/wikipedia/commons/e/e5/Venus-real_color.jpg' },
+      { title: 'Earth', url: 'https://en.wikipedia.org/wiki/Earth', image: 'https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg' },
+      { title: 'Mars', url: 'https://en.wikipedia.org/wiki/Mars', image: 'https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg' },
+      { title: 'Jupiter', url: 'https://en.wikipedia.org/wiki/Jupiter', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg' },
+      { title: 'Saturn', url: 'https://en.wikipedia.org/wiki/Saturn', image: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Saturn-27-03-04.jpeg' },
+      { title: 'Uranus', url: 'https://en.wikipedia.org/wiki/Uranus', image: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg' },
+      { title: 'Neptune', url: 'https://en.wikipedia.org/wiki/Neptune', image: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Neptune_Full.jpg' },
+      { title: 'Pluto', url: 'https://en.wikipedia.org/wiki/Pluto', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2a/Nh-pluto-in-true-color_2x_JPEG-edit-frame.jpg' },
+    ]
   }
 
   // Step 3: Add componentDidMount
@@ -40,17 +37,29 @@ class ArticleList extends Component {
     return (
       <FlatList
         style={styles.container}
-        data={this.props.planetData}
-        renderItem={({item}) => <ListItem title={item.title} />}
-        onPress={({item}) => this._showArticle(item)}
+        data={this.props.planets}
         keyExtractor={(item, index) => item.url}
+        renderItem={({item}) =>
+          <TouchableHighlight
+            onPress={() => { this._showArticle(item) }}>
+            <View
+              style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+              <Image
+                style={{width: 80, height: 80}}
+                source={{uri: item.image}}/>
+              <Text
+                style={{fontWeight: 'bold', fontSize: 17, margin: 20}}>
+                {item.title}
+              </Text>
+            </View>
+          </TouchableHighlight>}
       />
     )
   }
 
-  _showArticle(item) {
-    console.log("Show article with URL " + item.url)
-    this.props.navigation.navigate('Article', item)
+  _showArticle(data) {
+    console.log("Show article with URL " + data.url)
+    this.props.navigation.navigate('Article', data)
   }
 }
 
