082f27ecd39f2ec27a119899053d551d2930812d
Jimmy Dee <jgvdthree@gmail.com>
2019-07-26 12:41:38 -0700

    Import automation chnages from master branch

--- fastlane/lib/helper/update_helper.rb
+++ fastlane/lib/helper/update_helper.rb
@@ -1,6 +1,59 @@
+require 'cocoapods'
 require 'fileutils'
+require 'pathname'
 
 module UpdateHelper
+  UI = FastlaneCore::UI
+
+  def boolean_env_var?(varname, default_value: nil)
+    varstr = ENV[varname.to_s]
+    return default_value if varstr.nil?
+    varstr =~ /^[ty]/i
+  end
+
+  def pod_install_required?(podfile_path)
+    lockfile_path = "#{podfile_path}/Podfile.lock"
+    manifest_path = "#{podfile_path}/Pods/Manifest.lock"
+
+    return true unless File.readable?(lockfile_path) && File.readable?(manifest_path)
+
+    lockfile = Pod::Lockfile.from_file Pathname.new lockfile_path
+    manifest = Pod::Lockfile.from_file Pathname.new manifest_path
+
+    # diff the contents of Podfile.lock and Pods/Manifest.lock
+    # This is just what is done in the "[CP] Check Pods Manifest.lock" script build phase
+    # in a project using CocoaPods, but it also validates YAML parsing, etc.
+    return true unless lockfile == manifest
+
+    # Podfile must be evalled in its current directory in order to resolve
+    # the require_relative at the top.
+    podfile = Dir.chdir(podfile_path) do
+      # Why not just 'Podfile' or './Podfile'?
+      Pod::Podfile.from_file "#{podfile_path}/Podfile"
+    end
+
+    # compare checksum of Podfile with checksum in Podfile.lock in case Podfile
+    # updated since last pod install/update.
+    return true unless lockfile.to_hash["PODFILE CHECKSUM"] == podfile.checksum
+
+    false
+  rescue StandardError => e
+    UI.error e.message
+    true
+  end
+
+  def pod_install_if_required(podfile_path, verbose: false, repo_update: true)
+    install_required = pod_install_required?(podfile_path)
+    UI.message "pod install #{install_required ? '' : 'not '}required in #{podfile_path}"
+    return unless install_required
+
+    command = %w[pod install]
+    command << '--silent' unless verbose
+    command << '--repo-update' if repo_update
+
+    Dir.chdir(podfile_path) { Fastlane::Action.sh *command }
+  end
+
   def update_pods_in_tests_and_examples(params)
     # Updates to CocoaPods for unit tests and examples (requires
     # node_modules for each)
@@ -17,7 +70,7 @@ module UpdateHelper
       pods_folder = case folder
       when '.'
         'native-tests/ios'
-      when /_native_ios$/
+      when /webview_example_native_ios$/
         folder
       else
         "#{folder}/ios"
