1ba47d74d409709363cbbb606edf40ce78a500d7
Jimmy Dee <jimmy.dee@branch.io>
2020-03-30 16:04:40 -0700

    Update all Android example apps to use RNBranchModule.getAutoInstance

--- examples/testbed_native_android/android/app/src/main/java/io/branch/testbed_native_android/MainApplication.java
+++ examples/testbed_native_android/android/app/src/main/java/io/branch/testbed_native_android/MainApplication.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
-import io.branch.referral.Branch;
+import io.branch.rnbranch.RNBranchModule;
 
 /**
  * Created by jdee on 3/30/17.
@@ -14,7 +14,7 @@ public class MainApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        Branch.getAutoInstance(this);
+        RNBranchModule.getAutoInstance(this);
     }
 
     @Override
