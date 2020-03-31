a981d48ff69876259feb1413a710dd5e8be8b44e
Jimmy Dee <jimmy.dee@branch.io>
2020-03-31 11:00:28 -0700

    Use BranchSubscriber in branch.subscribe()

--- src/BranchSubscriber.js
+++ src/BranchSubscriber.js
@@ -21,25 +21,62 @@ export default class BranchSubscriber {
   options = {}
 
   /**
-   * Creates a new BranchSubscribe object with options.
+   * Creates a new BranchSubscriber object with options.
    *
    * @param {!Object} options an Object containing options
    */
   constructor(options) {
-    this.options = options
+    this.options = options || {}
+  }
+
+  subscribe() {
+    if (this.options.checkCachedEvents) {
+      RNBranch.redeemInitSessionResult().then((result) => {
+        if (result) {
+          /*** Cached value is returned, so set it as cached. ***/
+          if('params' in result && !!result['params']) {
+            result['params']['+rn_cached_initial_event'] = true
+          }
+
+          /*
+           * TODO: There can easily be a race here on cold launch from a link.
+           * An INIT_SESSION_START event can be emitted by the native layer
+           * before the subscribe() method is called in JS. Then subscribe() is
+           * called before a cached response is available, so here we get
+           * result == null. Then the live INIT_SESSION_SUCCESS event is
+           * transmitted to JS, and onOpenComplete is called with a non-null
+           * URI, but onOpenStart was never called. This can be addressed
+           * by caching the pending URI in the native layers.
+           */
+          if (this.options.onOpenStart && 'uri' in result && !!result['uri']) {
+            this.options.onOpenStart(result.uri)
+          }
+          if (this.options.onOpenComplete) {
+            this.options.onOpenComplete(result)
+          }
+        }
+
+        this._subscribe()
+      })
+    }
+    else {
+      this._subscribe()
+    }
   }
 
   /**
-   * Activates subscription to native events
+   * Activates subscription to native events. Private. Use
+   * subscribe() and set checkCachedEvents true or false in
+   * constructor.
    */
-  subscribe() {
-    if (options.onOpenStart) {
-      this._nativeEventEmitter.addListener(RNBranch.INIT_SESSION_START, options.onOpenStart)
+  _subscribe() {
+    if (this.options.onOpenStart) {
+      this._nativeEventEmitter.addListener(RNBranch.INIT_SESSION_START, this.options.onOpenStart)
     }
 
-    if (options.onOpenComplete) {
-      this._nativeEventEmitter.addListener(RNBranch.INIT_SESSION_SUCCESS, options.onOpenComplete)
-      this._nativeEventEmitter.addListener(RNBranch.INIT_SESSION_ERROR, options.onOpenComplete)
+    if (this.options.onOpenComplete) {
+      this._nativeEventEmitter.addListener(RNBranch.INIT_SESSION_SUCCESS, this.options.onOpenComplete)
+      this._nativeEventEmitter.addListener(RNBranch.INIT_SESSION_ERROR, this.options.onOpenComplete)
     }
   }
 
@@ -47,13 +84,13 @@ export default class BranchSubscriber {
    * Deactivates subscription to native events
    */
   unsubscribe() {
-    if (options.onOpenStart) {
-      this._nativeEventEmitter.removeListener(RNBranch.INIT_SESSION_START, options.onOpenStart)
+    if (this.options.onOpenStart) {
+      this._nativeEventEmitter.removeListener(RNBranch.INIT_SESSION_START, this.options.onOpenStart)
     }
 
-    if (options.onOpenComplete) {
-      this._nativeEventEmitter.removeListener(RNBranch.INIT_SESSION_SUCCESS, options.onOpenComplete)
-      this._nativeEventEmitter.removeListener(RNBranch.INIT_SESSION_ERROR, options.onOpenComplete)
+    if (this.options.onOpenComplete) {
+      this._nativeEventEmitter.removeListener(RNBranch.INIT_SESSION_SUCCESS, this.options.onOpenComplete)
+      this._nativeEventEmitter.removeListener(RNBranch.INIT_SESSION_ERROR, this.options.onOpenComplete)
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { NativeModules, NativeEventEmitter, DeviceEventEmitter, Platform } from 'react-native'
+import { NativeModules, Platform } from 'react-native'
 
-const { RNBranch, RNBranchEventEmitter } = NativeModules
+const { RNBranch } = NativeModules
 
 import createBranchUniversalObject from './branchUniversalObject'
 import BranchEvent from './BranchEvent'
+import BranchSubscribe from './BranchSubscriber'
 
 const packageFile = require('./../package.json')
 export const VERSION = packageFile.version
@@ -17,11 +18,6 @@ export const ShareCompletedEvent = RNBranch.SHARE_COMPLETED_EVENT
 export const ShareInitiatedEvent = RNBranch.SHARE_INITIATED_EVENT
 
 class Branch {
-  nativeEventEmitter = Platform.select({
-    android: DeviceEventEmitter,
-    ios: new NativeEventEmitter(RNBranchEventEmitter)
-  })
-
   key = null;
   _checkCachedEvents = true;
   _debug = false;
@@ -32,55 +28,19 @@ class Branch {
     console.info('Initializing react-native-branch v. ' + VERSION)
   }
 
-  subscribe(listener) {
-
-    /*
-     * If _checkCachedEvents flag is set, get the cached value from the native layer (asynchronously).
-     * If none, the listener is not called. If there is a cached value, it is passed to the listener.
-     */
-    if (this._checkCachedEvents) {
-      this._checkCachedEvents = false
-
-      RNBranch.redeemInitSessionResult().then((result) => {
-        if (result) {
-          /*** Cached value is returned, so set it as cached. ***/
-          if('params' in result && !!result['params']) {
-            result['params']['+rn_cached_initial_event'] = true
-          }
-
-          listener(result)
-        }
-
-        /*
-         * https://github.com/BranchMetrics/react-native-branch-deep-linking/issues/79
-         *
-         * By waiting until redeemInitSessionResult() returns, we roughly simulate a
-         * synchronous call to the native layer.
-         *
-         * Note that this is equivalent to
-         *
-         * let result = await RNBranch.redeemInitSessionResult()
-         * if (result) listener(result)
-         * this._addListener(listener)
-         *
-         * But by using then(), the subscribe method does not have to be async.
-         * This way, we don't add event listeners until the listener has received the
-         * initial cached value, which essentially eliminates all possibility of
-         * getting the same event twice.
-         */
-        this._addListener(listener)
-      })
-    }
-    else {
-      this._addListener(listener)
+  subscribe(options) {
+    if (typeof options === function) {
+      options = {
+        checkCachedEvents: this._checkCachedEvents,
+        onOpenComplete: options,
+      }
     }
 
-    // Initialize the native Branch SDK from JS
-    // -- Unsupportable on Android for the time being.
-    // RNBranch.initializeBranch(this.key)
+    const subscriber = new BranchSubscriber(options)
+    subscriber.subscribe()
 
     const unsubscribe = () => {
-      this._removeListener(listener)
+      subscriber.unsubscribe()
     }
 
     return unsubscribe
@@ -89,17 +49,6 @@ class Branch {
   skipCachedEvents() {
     /*** Sets to ignore cached events. ***/
     this._checkCachedEvents = false
-  }
-
-  _addListener(listener) {
-    // TODO: Add listener for INIT_SESSION_START
-    this.nativeEventEmitter.addListener(RNBranch.INIT_SESSION_SUCCESS, listener)
-    this.nativeEventEmitter.addListener(RNBranch.INIT_SESSION_ERROR, listener)
-  }
-
-  _removeListener(listener) {
-    this.nativeEventEmitter.removeListener(RNBranch.INIT_SESSION_SUCCESS, listener)
-    this.nativeEventEmitter.removeListener(RNBranch.INIT_SESSION_ERROR, listener)
   }
 
   /*** Tracking related methods ***/
