7d373a6f5d5b65661709e60cf8fb2391653af659
Jimmy Dee <jimmy.dee@branch.io>
2020-04-02 20:00:00 -0700

    Fleshing out RNBranchMock

--- test/RNBranchMock.js
+++ test/RNBranchMock.js
@@ -1,9 +1,27 @@
 import { NativeModules } from 'react-native'
 
 NativeModules.RNBranch = {
+  INIT_SESSION_START: 'RNBranch.initSessionStart',
+  INIT_SESSION_SUCCESS: 'RNBranch.initSessionSuccess',
+  INIT_SESSION_ERROR: 'RNBranch.initSessionError',
 
+  redeemInitSessionResult: async () => {
+    return {
+      params: {
+        '+clicked_branch_link': false,
+        '+is_first_session': false,
+      },
+      error: null,
+      uri: null,
+    }
+  },
 }
 
 NativeModules.RNBranchEventEmitter = {
+  addListener: (eventType) => {
 
+  },
+  removeListener: (count) => {
+
+  },
 }
