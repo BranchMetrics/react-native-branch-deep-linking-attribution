882992a511920733faf2a8fc6c136f016c9adeae
Jimmy Dee <jimmy.dee@branch.io>
2020-04-03 10:01:30 -0700

    Some tests using mocks

--- test/BranchSubscriber.test.js
+++ test/BranchSubscriber.test.js
@@ -1,14 +1,43 @@
+import { NativeModules } from 'react-native'
+const { RNBranch } = NativeModules
 import { BranchSubscriber } from 'react-native-branch'
 
-test('BranchSubscriber default initializes with no options', () => {
+test('default initializes with no options', () => {
   const subscriber = new BranchSubscriber(null)
   expect(subscriber.options).toEqual({})
 })
 
-test('BranchSubscriber stores options passed to the constructor', () => {
+test('stores options passed to the constructor', () => {
   const subscriber = new BranchSubscriber({
     checkCachedEvents: false
   })
 
   expect(subscriber.options).toEqual({ checkCachedEvents: false })
+})
+
+test('subscribes to init session start events', () => {
+  const subscriber = new BranchSubscriber({
+    checkCachedEvents: false,
+    onOpenStart: ({uri}) => {},
+  })
+  subscriber._nativeEventEmitter.addListener = jest.fn((eventType, listener) => {})
+  subscriber.subscribe()
+
+  expect(subscriber._nativeEventEmitter.addListener.mock.calls.length).toBe(1)
+  expect(subscriber._nativeEventEmitter.addListener.mock.calls[0][0]).toBe(RNBranch.INIT_SESSION_START)
+})
+
+test('subscribes to init session success & error events', () => {
+  const subscriber = new BranchSubscriber({
+    checkCachedEvents: false,
+    onOpenComplete: ({params, error, uri}) => {},
+  })
+  subscriber._nativeEventEmitter.addListener = jest.fn((eventType, listener) => {})
+  subscriber.subscribe()
+
+  expect(subscriber._nativeEventEmitter.addListener.mock.calls.length).toBe(2)
+
+  // TODO: This is brittle because it depends on the order.
+  expect(subscriber._nativeEventEmitter.addListener.mock.calls[0][0]).toBe(RNBranch.INIT_SESSION_SUCCESS)
+  expect(subscriber._nativeEventEmitter.addListener.mock.calls[1][0]).toBe(RNBranch.INIT_SESSION_ERROR)
 })
--- test/RNBranchMock.js
+++ test/RNBranchMock.js
@@ -5,23 +5,17 @@ NativeModules.RNBranch = {
   INIT_SESSION_SUCCESS: 'RNBranch.initSessionSuccess',
   INIT_SESSION_ERROR: 'RNBranch.initSessionError',
 
-  redeemInitSessionResult: async () => {
-    return {
-      params: {
-        '+clicked_branch_link': false,
-        '+is_first_session': false,
-      },
-      error: null,
-      uri: null,
-    }
-  },
+  redeemInitSessionResult: jest.fn(() => Promise.resolve({
+    params: {
+      '+clicked_branch_link': false,
+      '+is_first_session': false,
+    },
+    error: null,
+    uri: null,
+  })),
 }
 
 NativeModules.RNBranchEventEmitter = {
-  addListener: (eventType) => {
-
-  },
-  removeListener: (count) => {
-
-  },
+  addListener: jest.fn(eventType => {}),
+  removeListener: jest.fn(count => {}),
 }
