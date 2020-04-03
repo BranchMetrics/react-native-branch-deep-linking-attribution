1e9a8f3184801b88b96fc42d01a2aac29679f0e6
Jimmy Dee <jimmy.dee@branch.io>
2020-04-02 19:45:54 -0700

    Another BranchSubscriber test

--- test/BranchSubscriber.test.js
+++ test/BranchSubscriber.test.js
@@ -1,6 +1,16 @@
-import BranchSubscriber from '../src/BranchSubscriber'
+import { BranchSubscriber } from 'react-native-branch'
 
 test('BranchSubscriber default initializes with no options', () => {
   const subscriber = new BranchSubscriber(null)
   expect(typeof subscriber.options).toBe('object')
+})
+
+test('BranchSubscriber stores options passed to the constructor', () => {
+  const subscriber = new BranchSubscriber({
+    checkCachedEvents: false
+  })
+
+  expect(typeof subscriber.options).toBe('object')
+  expect('checkCachedEvents' in subscriber.options).toBe(true)
+  expect(subscriber.options.checkCachedEvents).toBe(false)
 })
