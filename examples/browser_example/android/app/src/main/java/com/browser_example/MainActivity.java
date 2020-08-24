f27d08bd8aeae002ee1e1fac03efdd18f2dda52c
Jimmy Dee <jimmy.dee@branch.io>
2020-08-24 15:24:38 -0700

    Rebuilt browser-example with RN 0.62.2

--- examples/browser_example/android/app/src/main/java/com/browser_example/MainActivity.java
+++ examples/browser_example/android/app/src/main/java/com/browser_example/MainActivity.java
@@ -8,35 +8,25 @@ import io.branch.rnbranch.RNBranchModule;
 
 public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "browser_example";
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "browser_example";
+  }
 
-    //*
-    @Override
-    protected void onStart() {
-        super.onStart();
-        RNBranchModule.initSession(getIntent().getData(), this);
-    }
-    // */
+  @Override
+  protected void onStart() {
+    super.onStart();
+    RNBranchModule.initSession(getIntent().getData(), this);
+  }
 
-    /*
-    @Override
-    protected void onResume() {
-        super.onResume();
-        RNBranchModule.initSession(getIntent().getData(), this);
-    }
-
-    // */
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        setIntent(intent);
-    }
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    RNBranchModule.onNewIntent(intent, this);
+    RNBranchModule.reInitSession(this);
+  }
 }
