941cf30d1bcab7ca20ea472cc315b02c7307bdea
Jimmy Dee <jimmy.dee@branch.io>
2020-08-25 10:19:06 -0700

    Add Share button and generally fix up browser_example

--- examples/browser_example/src/App.js
+++ examples/browser_example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native'
 import { WebView } from 'react-native-webview'
 
 import branch, { BranchEvent } from 'react-native-branch'
@@ -8,20 +8,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    marginTop: 64
+    marginTop: 64,
   },
   textInput: {
-    flex: 0.08
+    flex: 0.08,
   },
   webView: {
-    flex: 0.77
+    flex: 0.77,
+  },
+  button: {
+    backgroundColor: '#cceeee',
+    borderColor: '#2266aa',
+    borderTopWidth: 1,
+    flex: 0.15,
+    justifyContent: 'center',
   },
   buttonText: {
     color: '#2266aa',
     fontSize: 23,
     fontWeight: 'bold',
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
 })
 
 export default class App extends Component {
@@ -30,7 +37,7 @@ export default class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { text: 'Enter Branch URL', url: '' }
+    this.state = { text: 'https://branch.io', url: 'https://branch.io' }
   }
 
   componentDidMount() {
@@ -42,7 +49,12 @@ export default class App extends Component {
 
       console.log("Branch params: " + JSON.stringify(params))
 
-      if (!params['+clicked_branch_link']) return
+      if (!params['+clicked_branch_link']) {
+				if (!!params['+non_branch_link']) {
+					this.setState({url: params['+non_branch_link'], title: params['+non_branch_link']})
+				}
+				return
+			}
 
       // Get title and url for route
       let title = params.$og_title
@@ -51,7 +63,8 @@ export default class App extends Component {
 
       // Now reload the webview
       this.setState({url: url, title: title, image: image})
-    })
+    });
+		this.registerView();
   }
 
   componentWillUnmount() {
@@ -74,11 +87,23 @@ export default class App extends Component {
           style={styles.textInput}
           onChangeText={(text) => this.setState({text: text})}
           onEndEditing={this.editingEnded.bind(this)}
+					placeholder={'Enter Branch URL'}
+					autoCapitalize={'none'}
+					autoCompleteType={'off'}
+					autoCorrect={false}
           value={this.state.text} />
         <WebView
           style={styles.webView}
           source={{uri: this.state.url}}
           onLoad={this.registerView.bind(this)} />
+				<TouchableHighlight
+          onPress={this.onShare.bind(this)}
+          style={styles.button} >
+          <Text
+            style={styles.buttonText}>
+            Share
+          </Text>
+        </TouchableHighlight>
       </View>
     )
   }
@@ -99,5 +124,25 @@ export default class App extends Component {
     })
     this.buo.logEvent(BranchEvent.ViewItem)
     console.log("Created Branch Universal Object and logged standard view item event.")
+  }
+
+  async onShare() {
+    let { channel, completed, error } = await this.buo.showShareSheet({
+      emailSubject: this.state.title,
+      messageHeader: this.state.title,
+    }, {
+      feature: "share",
+      channel: "RNApp",
+    }, {
+      $desktop_url: this.state.url,
+      $ios_deepview: "branch_default",
+    });
+
+    if (error) {
+      console.error("Error sharing via Branch: " + error);
+      return;
+    }
+
+    console.log("Share to " + channel + " completed: " + completed);
   }
 }
