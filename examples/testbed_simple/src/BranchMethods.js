64244cf6542bc076dcbd125252474d355ce5dfdb
vikasjain1979 <vikas_jain2012@yahoo.com>
2022-07-14 15:42:28 +1000

    Removed Extra space
    Removed Extra space

--- examples/testbed_simple/src/BranchMethods.js
+++ examples/testbed_simple/src/BranchMethods.js
@@ -1,4 +1,4 @@
- import React, { Component } from 'react'
+import React, { Component } from 'react'
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
 
 import Button from './Button'
