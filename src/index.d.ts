e9ad44075e75db29f233a50b2a7ffc3d453807fc
hojunlee <zeallat94@gmail.com>
2022-11-02 03:21:34 +0900

    Fix BranchUniversalObject#generateShortUrl parameter type definition

--- src/index.d.ts
+++ src/index.d.ts
@@ -296,8 +296,8 @@ interface BranchUniversalObject {
     controlParams?: BranchLinkControlParams
   ) => Promise<BranchShareSuccess | BranchShareFailure>;
   generateShortUrl: (
-    linkProperties: BranchLinkProperties,
-    controlParams: BranchLinkControlParams
+    linkProperties?: BranchLinkProperties,
+    controlParams?: BranchLinkControlParams
   ) => Promise<{ url: string }>;
   logEvent: (eventName: string, params?: BranchEventParams) => Promise<null>;
   release: () => void;
